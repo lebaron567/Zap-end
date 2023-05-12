0bb5f11bd7ba79206a1caaa42307e7ddd5c97554
lrichard <Rayce16@laposte.net>
2023-05-12 15:09:48 +0200

    set up basic page

--- server.go
+++ server.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os/exec"
+)
 
+func main() {
+	http.HandleFunc("/", Home)
+	go exec.Command("cmd", "/C", "start", "http://localhost:8080").Run()
+	http.ListenAndServe(":8080", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/home.html")
 }
