a16a77503c2a7f87cfb837f3c15f03aa6ae81bf9
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-05-25 13:55:20 +0200

    set infrastruture

--- server.go
+++ server.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os/exec"
+	"text/template"
 )
 
+var home = template.Must(template.ParseFiles("template/home.html"))
+var registration = template.Must(template.ParseFiles("template/registration.html"))
+var explorer = template.Must(template.ParseFiles("template/Explorer.html"))
+var message = template.Must(template.ParseFiles("template/message.html"))
+var profil = template.Must(template.ParseFiles("template/profil.html"))
+var ff = 0
+
 func main() {
-	http.HandleFunc("/", Home)
-	go exec.Command("cmd", "/C", "start", "http://localhost:8080").Run()
+	http.HandleFunc("/home", Home)
+	http.HandleFunc("/registration", Registration)
+	http.HandleFunc("/explorer", Explorer)
+	http.HandleFunc("/message", Message)
+	http.HandleFunc("/profil", Profil)
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	fmt.Println("Serveur start at : http://localhost:8080/home")
 	http.ListenAndServe(":8080", nil)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "template/home.html")
+	err := home.Execute(w, ff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func Registration(w http.ResponseWriter, r *http.Request) {
+	err := registration.Execute(w, ff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+func Explorer(w http.ResponseWriter, r *http.Request) {
+	err := explorer.Execute(w, ff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+func Message(w http.ResponseWriter, r *http.Request) {
+	err := message.Execute(w, ff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+func Profil(w http.ResponseWriter, r *http.Request) {
+	err := profil.Execute(w, ff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
