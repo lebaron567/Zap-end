75a6c020364488cd28a9c46c6a40d18643e38ae5
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-05-30 15:31:57 +0200

    merge

--- back/main.go
+++ back/main.go
@@ -20,10 +20,10 @@ func main() {
 	// fmt.Println(value)
 
 	// back.AddUser(18, "b", "b", "a@y.c", "secret", "a")
-	value := back.GetAllFeildInTable("users")
-	fmt.Println(value)
-	fmt.Println(value[0][6])
-	fmt.Println(value[1][6])
+	//value := back.GetAllFeildInTable("users")
+	//fmt.Println(value)
+	//fmt.Println(value[0][6])
+	//fmt.Println(value[1][6])
 
 	http.HandleFunc("/home", back.Home)
 	http.HandleFunc("/registration", back.Registration)
