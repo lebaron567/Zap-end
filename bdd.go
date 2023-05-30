7c9741cce87648a8fcafb6e5686f52e1c0579fa9
arthurLacombe <arthurlacombe44@gmail.com>
2023-05-30 14:16:17 +0200

    simplify reading

--- bdd.go
+++ bdd.go
@@ -24,14 +24,14 @@ func InitBDD() {
 	database := OpenBDD()
 	defer database.Close()
 	tmp := `
-	CREATE TABLE IF NOT EXISTS "users" (
-		"id"	INTEGER NOT NULL UNIQUE,
-		"age"	INTEGER NOT NULL,
-		"firstname_users"	VARCHAR(20) NOT NULL,
-		"lastname_users"	VARCHAR(30) NOT NULL,
-		"email_users"		VARCHAR(50) NOT NULL UNIQUE,
-		"password_hashed_users"	VARCHAR(45) NOT NULL,
-		"pseudo_users"	VARCHAR(20) NOT NULL UNIQUE,
+	CREATE TABLE IF NOT EXISTS "user" (
+		"id"					INTEGER NOT NULL UNIQUE,
+		"age"					INTEGER NOT NULL,
+		"firstname_user"		VARCHAR(20) NOT NULL,
+		"lastname_user"			VARCHAR(30) NOT NULL,
+		"email_user"			VARCHAR(50) NOT NULL UNIQUE,
+		"password_hashed_user"	VARCHAR(45) NOT NULL,
+		"pseudo_user"	V		ARCHAR(20) NOT NULL UNIQUE,
 		PRIMARY KEY("id" AUTOINCREMENT)
 		
 	);
