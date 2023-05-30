a23ef8d46ae16275430a66d78103ce0673f8a8d1
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-05-30 15:29:55 +0200

    merge BDD

--- back/main.go
+++ back/main.go
@@ -3,7 +3,16 @@ package main
 import (
 	"back"
 	"fmt"
+	"html/template"
+	"net/http"
 )
+
+var home = template.Must(template.ParseFiles("template/home.html"))
+var registration = template.Must(template.ParseFiles("template/registration.html"))
+var explorer = template.Must(template.ParseFiles("template/Explorer.html"))
+var message = template.Must(template.ParseFiles("template/message.html"))
+var profil = template.Must(template.ParseFiles("template/profil.html"))
+var ff = 0
 
 func main() {
 	back.InitBDD()
@@ -16,4 +25,13 @@ func main() {
 	fmt.Println(value[0][6])
 	fmt.Println(value[1][6])
 
+	http.HandleFunc("/home", back.Home)
+	http.HandleFunc("/registration", back.Registration)
+	http.HandleFunc("/explorer", back.Explorer)
+	http.HandleFunc("/message", back.Message)
+	http.HandleFunc("/profil", back.Profil)
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	fmt.Println("Serveur start at : http://localhost:8080/home")
+	http.ListenAndServe(":8080", nil)
 }
