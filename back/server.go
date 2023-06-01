38998cf31401d978e13a5651ceb863d588d4a975
arthurLacombe <arthurlacombe44@gmail.com>
2023-06-01 14:46:47 +0200

    change packages

--- back/server.go
+++ back/server.go
@@ -1,6 +1,7 @@
-package back
+package main
 
 import (
+	"back"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 
 var home = template.Must(template.ParseFiles("template/home.html"))
 var registration = template.Must(template.ParseFiles("template/registration.html"))
+var connexion = template.Must(template.ParseFiles("template/connexion.html"))
 var explorer = template.Must(template.ParseFiles("template/Explorer.html"))
 var message = template.Must(template.ParseFiles("template/message.html"))
 var profil = template.Must(template.ParseFiles("template/profil.html"))
@@ -21,6 +23,8 @@ func main() {
 	http.HandleFunc("/explorer", Explorer)
 	http.HandleFunc("/message", Message)
 	http.HandleFunc("/profil", Profil)
+	http.HandleFunc("/connexion", Connexion)
+
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	fmt.Println("Serveur start at : http://localhost:8080/home")
@@ -29,6 +33,31 @@ func main() {
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := home.Execute(w, ff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func Connexion(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		var password_hashed_user string
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		database := back.OpenBDD()
+		rows, err := database.Query("SELECT password_hashed_user FROM user WHERE email_user = " + email)
+		if err != nil {
+			fmt.Print(err)
+		}
+		rows.Scan(&password_hashed_user)
+		if back.CheckPasswordHash(password, password_hashed_user) {
+			// acces authoriser
+		} else {
+			// acces refuser
+		}
+
+	}
+	err := connexion.Execute(w, ff)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -47,7 +76,7 @@ func Registration(w http.ResponseWriter, r *http.Request) {
 			log.Fatal("strconv issue")
 		}
 		fmt.Println(prenom, nom, email, pseudo, password, age)
-		bdderr := AddUser(age, prenom, nom, email, password, pseudo)
+		bdderr := back.AddUser(age, prenom, nom, email, password, pseudo)
 		if bdderr != nil {
 			// affichier l'erreur a l'utilsateur
 		}
